196d5a2e6eac039f23d9a031f14b3ccf0c226b84
MRHenriq <marissa.henriquez@gmail.com>
2025-07-26 18:37:03 -0700

    ?

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -5,7 +5,7 @@
 
 // sortUserName - a function that takes user input and sorts the letters of their name. 
 function sortUserName() {
-   var userName = window.prompt ("Hi. Please tell me your name so I can fix it.");
+   var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
     console.log("userName =", userName);
 
   // split string to array
