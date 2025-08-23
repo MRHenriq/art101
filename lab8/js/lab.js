78ce54ee20cbbda1327863a315df06b2433bc2c4
MRHenriq <marissa.henriquez@gmail.com>
2025-08-22 18:27:59 -0700

    Update lab.js

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,26 +1,31 @@
 // Author: Marissa Henriquez <mrhenriq@ucsc.edu>
-// Date: Jul 8
+// Date: August 8
 
+array = [5, 14, 26, 38, 44, 68]
+console.log("My array", array);
 
-// Define Variables
-myTransport = ["car","walking"];
-
-//Create and Object for my main ride
-myMainRide = {
-  make : "Toyota",
-  model : "Camry",
-  color : "Silver",
-  year : 2012,
-  age : function() {
-    return 2025 - this.year;
+function addition(x) {
+   var results = x + 7; 
+   return results; 
   }
 
-}
+//test function
+console.log("Is 5 + 7 = 12?", addition(5));
+console.log("Is 14 + 7 = 21?", addition(14));
 
-// Output
-document.writeln("Kinds of tranportation I use: ", myTransport, "</br>");
+array.map(addition); 
+console.log("Test of addition of array:", result);
+
+array.map(function(x){ 
+  var results = x - 2; 
+  return results; 
+})
+console.log("Test of array - 2", result);
+
+var mapResults = array.map(addition); 
+console.log("Results: ", mapResults)
 
 
-document.writeln("My Main Ride: <pre>", 
-  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
 
