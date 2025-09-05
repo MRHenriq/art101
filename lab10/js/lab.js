34de3e767bb2117024c813a164a1898c4de39348
MRHenriq <marissa.henriquez@gmail.com>
2025-09-05 12:48:43 -0700

    lab10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,26 +1,25 @@
 // Author: Marissa Henriquez <mrhenriq@ucsc.edu>
-// Date: Jul 8
+// Date: September 1
 
-
-// Define Variables
-myTransport = ["car","walking"];
-
-//Create and Object for my main ride
-myMainRide = {
-  make : "Toyota",
-  model : "Camry",
-  color : "Silver",
-  year : 2012,
-  age : function() {
-    return 2025 - this.year;
-  }
-
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
 }
 
-// Output
-document.writeln("Kinds of tranportation I use: ", myTransport, "</br>");
+// click listener for button
+$("#make-convo").click(function(){
+  // get new fake dialogue
+  const newText = generateRandomText();
+  //append a new div to our output div
+  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
 
 
-document.writeln("My Main Ride: <pre>", 
-  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
 
